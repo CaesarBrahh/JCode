44f66332487651bf09496253f707d3c419db97f2
CaesarBrahh <julaenw@gmail.com>
2026-05-20 00:29:06 -0500

    conversation history maintenance

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 def main():
     # load in api key from .env file
@@ -13,19 +14,21 @@ def main():
     # generate ai client
     client = genai.Client(api_key=api_key)
     
-    # initialize argparse for user input
+    # collect user input
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
+    # response
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=args.user_prompt
+        model='gemini-2.5-flash', contents=messages
     )
 
     if response.usage_metadata == None:
         raise RuntimeError("failed api request")
     
-    print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
