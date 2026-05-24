592a1d17138c1e9c813876a3ceee18b3b167b2e2
CaesarBrahh <julaenw@gmail.com>
2026-05-23 21:26:40 -0500

    agent running

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 from google.genai import types
 from prompts import system_prompt
 from call_function import available_functions, call_function
+import sys
 
 def main():
     # load in api key from .env file
@@ -30,21 +31,40 @@ def main():
     # agent loop
     for _ in range(20):
         # collect response
-        response = generate_response(messages)
+        response = generate_response(messages, client)
+
+        if response.usage_metadata == None:
+            raise RuntimeError("failed api request")
 
         # append 
         for candidate in response.candidates:
-            messages.append([types.Content(role="user", parts=[types.Part(text=candidate.content)])])
+            messages.append(candidate.content)
 
-    if response.usage_metadata == None:
-        raise RuntimeError("failed api request")
+        # call the functions
+        function_responses = collect_function_call_responses(response, args)
+
+        # break out loop if no function_responses
+        if not function_responses:
+            break
+
+        # append function results to messages list
+        messages.append(types.Content(role="user", parts=function_responses))
+
+    # max iterations reached, but no final response
+    else:
+        print("Max iterations ran without reaching a final response.")
+        sys.exit(1)
 
     # output
     if args.verbose == True:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
+    print(f"Response:\n{response.text}")
+
+def collect_function_call_responses(response, args):
     function_results = []
+
     if response.function_calls:
         for function_call in response.function_calls:
             # call function
@@ -59,15 +79,16 @@ def main():
                 raise Exception("no response to function response found")
 
             # append function call response to a list of function results
-            function_results.append(function_call_result.parts[0].function_response.response)
+            function_results.append(function_call_result.parts[0])
 
             # verbose
             if args.verbose:
                 print(f"-> {function_call_result.parts[0].function_response.response}")
+                
+    return function_results
 
-    print(f"Response:\n{response.text}")
 
-def generate_response(messages):
+def generate_response(messages, client):
     return client.models.generate_content(
         model='gemini-2.5-flash', 
         contents=messages,
